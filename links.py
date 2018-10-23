3b51fb3717d3f83071b5edbedc5c4ad46dbfac29
dannyzen <danny.rosen@gmail.com>
2018-10-23 10:04:52 -0400

    adding a redirect sanitizer

--- links.py
+++ links.py
@@ -5,6 +5,9 @@ import json
 import os
 from string import Template
 import yaml
+import urlparse
+import httplib
+
 
 try:
     config = yaml.load(file('config.yaml', 'r'))
@@ -16,12 +19,30 @@ url = "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key="
 headers = {'content-type': 'application/json'}
 
 def get_short_link(web_link):
+    resolve_http_redirect(web_link)
     payload = {"dynamicLinkInfo":{"dynamicLinkDomain": "tamnews.page.link","link": web_link, "navigationInfo": { "enableForcedRedirect": "1"}, }, "suffix": { "option": "SHORT"}}
     response = requests.post( url+key, data=json.dumps(payload), headers=headers)
     extraction = json.loads(response.text)
     # print(extraction)
     short_link = extraction['shortLink']
     return short_link
+
+def resolve_http_redirect(url, depth=0):
+""" Recursively follow redirects until there isn't a location header """
+    if depth > 10:
+        raise Exception("Redirected "+depth+" times, giving up.")
+    o = urlparse.urlparse(url,allow_fragments=True)
+    conn = httplib.HTTPConnection(o.netloc)
+    path = o.path
+    if o.query:
+        path +='?'+o.query
+    conn.request("HEAD", path)
+    res = conn.getresponse()
+    headers = dict(res.getheaders())
+    if headers.has_key('location') and headers['location'] != url:
+        return resolve_http_redirect(headers['location'], depth+1)
+    else:
+        return url
 
 def append_template_to_file(link, linkText, description):
     f=open("content.html", "a+")
